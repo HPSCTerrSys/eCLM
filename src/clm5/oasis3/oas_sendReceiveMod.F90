3c1643f4822ca3f4265302ef26e7a50bfc2dc5e2
kvrigor <kvrigor@users.noreply.github.com>
2022-01-14 10:04:46 +0100

    Fixes for incorrect coupling field data (#10)
    Commit summary:
    * 71baab0 Fixed wrong domain decomposition by switching to ORANGE partitioning scheme
    * 6a0fce3 Fixed syntax for checking missing dict keys
    * 1bea6fd Changed to bundled coupling fields (w/ extra dim = # of soil levels)
    * fb6b252 Always compile with OpenMP.
    * 906ff7b Fixed mismatched coupling timestep with ParFlow

--- src/clm5/oasis3/oas_sendReceiveMod.F90
+++ src/clm5/oasis3/oas_sendReceiveMod.F90
@@ -1,8 +1,9 @@
 module oas_sendReceiveMod
   use shr_kind_mod     , only: r8 => shr_kind_r8
-  use clm_time_manager , only: get_curr_time, get_prev_time
+  use clm_time_manager , only: get_nstep, get_step_size
   use decompMod        , only: bounds_type
   use clm_varpar       , only: nlevsoi
+  use clm_varctl       , only: iulog
   use oas_vardefMod
   use mod_oasis
   implicit none
@@ -11,40 +12,40 @@ module oas_sendReceiveMod
 
   public  :: oas_send
   public  :: oas_receive
-  integer :: days_elapsed, seconds_elapsed
-  integer :: n_grid_points
-  integer :: i, ierror
   
 contains
 
-  subroutine oas_receive(bounds, atm2lnd_inst)
+  subroutine oas_receive(bounds, seconds_elapsed, atm2lnd_inst)
     use atm2lndType, only: atm2lnd_type
+
     type(bounds_type),  intent(in)    :: bounds
+    integer          ,  intent(in)    :: seconds_elapsed
     type(atm2lnd_type), intent(inout) :: atm2lnd_inst
-    real(kind=r8),      allocatable   :: buffer(:)
-    
-    n_grid_points = (bounds%endg - bounds%begg) + 1
-    allocate(buffer(n_grid_points))
-    call get_curr_time(days_elapsed, seconds_elapsed)    
-    do i = 1, nlevsoi
-      call oasis_get(psi(i)%id, seconds_elapsed, buffer, ierror)
-      atm2lnd_inst%parflow_psi_grc(:,i) = buffer
-    end do
+    real(kind=r8),      allocatable   :: buffer(:,:)
+    integer                           :: num_grid_points
+    integer                           :: info
+
+    num_grid_points = (bounds%endg - bounds%begg) + 1
+    allocate(buffer(num_grid_points, nlevsoi))
+
+    call oasis_get(oas_psi_id, seconds_elapsed, atm2lnd_inst%parflow_psi_grc, info)
+    call oasis_get(oas_sat_id, seconds_elapsed, buffer, info)
+
   end subroutine oas_receive
 
-  subroutine oas_send(bounds, lnd2atm_inst)
+  subroutine oas_send(bounds, seconds_elapsed, lnd2atm_inst)
     use lnd2atmType, only : lnd2atm_type
-    type(bounds_type),  intent(in)    :: bounds
-    type(lnd2atm_type), intent(inout) :: lnd2atm_inst
-    real(kind=r8),      allocatable   :: buffer(:)
+    use spmdMod,     only : mpicom
+    use shr_mpi_mod, only: shr_mpi_barrier
 
-    n_grid_points = (bounds%endg - bounds%begg) + 1
-    allocate(buffer(n_grid_points))
-    call get_prev_time(days_elapsed, seconds_elapsed)
-    do i = 1, nlevsoi
-      buffer = lnd2atm_inst%qflx_parflow_grc(:,i)
-      call oasis_put(et_loss(i)%id, seconds_elapsed, buffer, ierror)
-    end do
+    type(bounds_type),  intent(in)    :: bounds
+    integer          ,  intent(in)    :: seconds_elapsed
+    type(lnd2atm_type), intent(inout) :: lnd2atm_inst
+
+    integer                           :: info
+    
+    call oasis_put(oas_et_loss_id, seconds_elapsed, lnd2atm_inst%qflx_parflow_grc, info)
+    
   end subroutine oas_send
 
 end module oas_sendReceiveMod
--- src/clm5/oasis3/oas_vardefMod.F90
+++ src/clm5/oasis3/oas_vardefMod.F90
@@ -2,17 +2,6 @@ module oas_vardefMod
   implicit none
   save
   
-  ! Type for coupling field information
-  type :: oas_var
-    character(len=12) :: name  ! Name of the coupling field
-    integer           :: id    ! Id of the field
-  end type oas_var
-
-  ! Sent fields
-  type(oas_var), allocatable :: et_loss(:) ! soil ET loss
-
-  ! Received fields from Parflow
-  type(oas_var), allocatable :: watsat(:)  ! water saturation
-  type(oas_var), allocatable :: psi(:)     ! pressure head
+  integer :: oas_psi_id, oas_et_loss_id, oas_sat_id
 
 end module oas_vardefMod
