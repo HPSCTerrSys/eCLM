9870b27deac079882b365a01eb634d6a48c3cc3d
kvrigor <kvrigor@users.noreply.github.com>
2022-04-20 10:47:12 +0200

    Removed optional argument `var_noshape` in oasis_def_var
    Starting OASIS3-MCT v5.0, `var_noshape` can be removed.

--- src/clm5/oasis3/oas_defineMod.F90
+++ src/clm5/oasis3/oas_defineMod.F90
@@ -11,7 +11,7 @@ contains
   subroutine oas_definitions_init(bounds)
     use spmdMod      , only : masterproc
     use clm_varpar   , only : nlevsoi, nlevgrnd
-    use decompMod    , only : bounds_type
+    use decompMod    , only : ldecomp, bounds_type
     use oas_vardefMod
 
     type(bounds_type) , intent(in)  :: bounds ! start and end gridcell indices for this MPI task
@@ -25,7 +25,6 @@ contains
 
     ! oasis_def_var
     integer              :: var_nodims(2)     ! var dimension parameters
-    integer              :: var_shape(1)      ! unused dummy input to oasis_def_var
 
     if (masterproc) then
       call define_grid()
@@ -74,11 +73,11 @@ contains
     var_nodims(1) = 1         ! unused
     var_nodims(2) = nlevsoi   ! number of fields in a bundle
 
-    call oasis_def_var(oas_et_loss_id, "ECLM_ET", grid_id, var_nodims, OASIS_Out, var_shape, OASIS_Real, ierror) 
+    call oasis_def_var(oas_et_loss_id, "ECLM_ET", grid_id, var_nodims, OASIS_Out, OASIS_Real, ierror) 
     
     var_nodims(2) = nlevgrnd         ! number of fields in a bundle
-    call oasis_def_var(oas_sat_id, "ECLM_SAT", grid_id, var_nodims, OASIS_In, var_shape, OASIS_Real, ierror)
-    call oasis_def_var(oas_psi_id, "ECLM_PSI", grid_id, var_nodims, OASIS_In, var_shape, OASIS_Real, ierror)
+    call oasis_def_var(oas_sat_id, "ECLM_SAT", grid_id, var_nodims, OASIS_In, OASIS_Real, ierror)
+    call oasis_def_var(oas_psi_id, "ECLM_PSI", grid_id, var_nodims, OASIS_In, OASIS_Real, ierror)
 
     ! End definition phase
     call oasis_enddef(ierror)
