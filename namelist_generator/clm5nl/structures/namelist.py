bb5caa29f521e7c173069e1028f44c7267455884
kvrigor <kvrigor@users.noreply.github.com>
2022-07-25 18:26:39 +0200

    Made reading/writing/checking valid namelist parameters more convenient
    Summary of changes:
    
    * namelist_group:
      - Turned `valid_params` into a dict so that it could be
        passed to NamelistGroupMixin and then ultimately to the
        parent Namelist object.
    * Namelist:
      - Added new internal dictionary _valid_params which contains all
        supported namelist parameters. This allows an easy way of checking
        if a namelist parameter is supported (i.e. `"param" in _nl`)
      - Implemented __getitem__ and __setitem__. This enables convenient
        reading (`value = _nl["param"]`) and writing (`_nl["param"] = value`)
        namelist parameters without needing to know which group it belongs to.
    * NamelistGroupMixin:
      - Replaced _set_param, _get_param, and _del_param
        with property descriptors __get__, __set__, __delete__
      - `nl_obj` is passed to the __init__ constructor instead of
        manually setting it (i.e. `self._group._parent = nl_obj`)
    * namelist_item:
      - Used the new property descriptors of NamelistGroupMixin

--- namelist_generator/clm5nl/structures/namelist.py
+++ namelist_generator/clm5nl/structures/namelist.py
@@ -4,6 +4,7 @@ from .utils import nml2str, nmlGroup2str
 class Namelist(object):
     def __init__(self, nl_obj: dict = None):
         self._name = type(self).__name__
+        self._valid_params = {}
         if nl_obj is None:
             self._nl = OrderedDict()
         else:
@@ -13,10 +14,28 @@ class Namelist(object):
         return self._nl.__iter__()
 
     def __len__(self):
-        acc = 0
-        for v in self._nl.values():
-            acc += len(v)
-        return acc
+        param_count = 0
+        for nl_group in self._nl.values():
+            param_count += len(nl_group)
+        return param_count
+
+    def __getitem__(self, key):
+        if key in self._valid_params:
+            return self._nl[self._valid_params[key]][key]
+        else:
+            raise KeyError(f"'{key}' is not a valid {self._name} parameter.")
+
+    def __setitem__(self, key, value):
+        if key in self._valid_params:
+            self._nl[self._valid_params[key]][key] = value
+        else:
+            raise KeyError(f"'{key}' is not a valid {self._name} parameter.")
+
+    def __contains__(self, item):
+        return item in self._valid_params.keys()
+
+    def _update_valid_params(self, valid_params: dict):
+        self._valid_params.update(valid_params)
 
     def keys(self):
         return self._nl.keys()
@@ -29,22 +48,24 @@ class Namelist(object):
             f.write(nml2str(self._nl, grp_names=grp_names))
 
 class NamelistGroupMixin(object):
-    def __init__(self, valid_params: list[str]):
+    def __init__(self, parent: Namelist, valid_params: dict):
         self._group_name = type(self).__name__
-        self._parent: Namelist = None
+        self._parent = parent
         self._valid_params = valid_params
+        self._parent._update_valid_params(valid_params)
 
-    def _get_param(self, key):
+    def __getitem__(self, key):
         if self._group_name in self._parent._nl:
             return self._parent._nl[self._group_name].get(key, None)
         else:
             return None
         
-    def _set_param(self, key, value):
-        if not self._group_name in self._parent._nl: self._parent._nl[self._group_name] = OrderedDict()
+    def __setitem__(self, key, value):
+        if not self._group_name in self._parent._nl: 
+            self._parent._nl[self._group_name] = OrderedDict()
         self._parent._nl[self._group_name][key] = value
 
-    def _del_param(self, key):
+    def __delitem__(self, key):
         if self._group_name in self._parent._nl:
            del self._parent._nl[self._group_name][key]
 
@@ -61,7 +82,7 @@ class NamelistGroupMixin(object):
             return {}.__iter__()
 
     def __contains__(self, item):
-        return item in self._valid_params
+        return item in self._valid_params.keys()
 
     def __len__(self):
         if self._group_name in self._parent._nl:
@@ -70,17 +91,17 @@ class NamelistGroupMixin(object):
             return 0 
 
     def __str__(self):
-        return nmlGroup2str(self._group_name, {p:self._get_param(p) for p in self._valid_params})
+        return nmlGroup2str(self._group_name, {p:self[p] for p in self._valid_params})
 
 class namelist_group():
     def __init__(self, nl_grp):
         self._group_type = type(nl_grp.__name__, (nl_grp, NamelistGroupMixin), {})
         self._group = None
-        self._params = [p for p in dir(nl_grp) if not p.startswith('_')]
+        self._params = {p:nl_grp.__name__ for p in dir(nl_grp) if not p.startswith('_')}
 
     def __get__(self, nl_obj, objtype):
-        if self._group is None: self._group = self._group_type(valid_params = self._params)
-        self._group._parent = nl_obj
+        if self._group is None: 
+            self._group = self._group_type(parent = nl_obj, valid_params = self._params)
         return self._group
 
 class namelist_item(object):
@@ -89,10 +110,10 @@ class namelist_item(object):
         self.__doc__ = func.__doc__
 
     def __get__(self, nl_grp, objtype):
-        return nl_grp._get_param(self._param_name)
+        return nl_grp[self._param_name]
 
     def __set__(self, nl_grp, value):
-        nl_grp._set_param(self._param_name, value)
+        nl_grp[self._param_name] = value
 
     def __delete__(self, nl_grp):
-        nl_grp._del_param(self._param_name)
+        del nl_grp[self._param_name]
