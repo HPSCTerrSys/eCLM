27d984d896505e94fe10c4182b9ab55acd8c1a08
Stefan Poll <s.poll@fz-juelich.de>
2022-08-14 22:37:21 +0200

    Update snd/rcv fields from ICON

--- src/clm5/oasis3/oas_sendReceiveMod.F90
+++ src/clm5/oasis3/oas_sendReceiveMod.F90
@@ -59,6 +59,7 @@ contains
     real(kind=r8),      allocatable   :: buffer(:,:)
     integer                           :: num_grid_points
     integer                           :: info
+    integer                           :: g
 
     integer ::   jps_t   =  1            ! temperature
     integer ::   jps_u   =  2            ! u wind
@@ -73,10 +74,7 @@ contains
     integer ::   jps_gr  = 11            ! convective rain + snow precipitation      (kg/m2*s)
 
     num_grid_points = (bounds%endg - bounds%begg) + 1
-    allocate(buffer(num_grid_points, nlevsoi))
-
-    call oasis_get(oas_psi_id, seconds_elapsed, atm2lnd_inst%parflow_psi_grc, info)
-    call oasis_get(oas_sat_id, seconds_elapsed, buffer, info)
+    allocate(buffer(num_grid_points, 1))
 
     !    call oasis_get(jps_t, seconds_elapsed, oas_rcv_meta(:,:,jps_t), info)
     call oasis_get(jps_t,  seconds_elapsed, atm2lnd_inst%forc_t_not_downscaled_grc, info)
@@ -84,14 +82,23 @@ contains
     call oasis_get(jps_v,  seconds_elapsed, atm2lnd_inst%forc_v_grc, info)
     call oasis_get(jps_qv, seconds_elapsed, atm2lnd_inst%forc_q_not_downscaled_grc, info)
     call oasis_get(jps_ht, seconds_elapsed, atm2lnd_inst%forc_hgt_grc, info)
-    call oasis_get(jps_pr, seconds_elapsed, buffer, info)
-    call oasis_get(jps_rs, seconds_elapsed, buffer, info)
-    call oasis_get(jps_fs, seconds_elapsed, buffer, info)
+    call oasis_get(jps_pr, seconds_elapsed, atm2lnd_inst%forc_pbot_not_downscaled_grc, info)
+    call oasis_get(jps_rs, seconds_elapsed, atm2lnd_inst%forc_solad_grc(:,1), info)
+    call oasis_get(jps_fs, seconds_elapsed, atm2lnd_inst%forc_solai_grc(:,1), info)
     call oasis_get(jps_lw, seconds_elapsed, atm2lnd_inst%forc_lwrad_not_downscaled_grc, info)
     call oasis_get(jps_cr, seconds_elapsed, atm2lnd_inst%forc_rain_not_downscaled_grc, info)
     call oasis_get(jps_gr, seconds_elapsed, buffer, info)
 
-    !SPo: postprocessing of atm2lnd is missing, better use x2l 
+    !SPo: some postprocessing of atm2lnd is missing; may better use x2l 
+
+    do g=bounds%begg,bounds%endg
+       atm2lnd_inst%forc_solad_grc(g,1) = 0.5_r8 * atm2lnd_inst%forc_solad_grc(g,1)
+       atm2lnd_inst%forc_solad_grc(g,2) = atm2lnd_inst%forc_solad_grc(g,1)
+       atm2lnd_inst%forc_solai_grc(g,1) = 0.5_r8 * atm2lnd_inst%forc_solai_grc(g,1)
+       atm2lnd_inst%forc_solai_grc(g,2) = atm2lnd_inst%forc_solai_grc(g,1)
+       atm2lnd_inst%forc_solar_grc(g)   =  atm2lnd_inst%forc_solad_grc(g,2) + atm2lnd_inst%forc_solad_grc(g,1) &
+                                        +  atm2lnd_inst%forc_solai_grc(g,2) + atm2lnd_inst%forc_solai_grc(g,1)
+    enddo
 
   end subroutine oas_receive_icon
 
@@ -108,21 +115,16 @@ contains
     integer                           :: info
 
     num_grid_points = (bounds%endg - bounds%begg) + 1
-    allocate(aux_buffer(num_grid_points, nlevsoi))
+    allocate(aux_buffer(num_grid_points, 1))
 
-!    call oasis_put(oas_et_loss_id, seconds_elapsed, lnd2atm_inst%qflx_parflow_grc, info)
-     call oasis_put(1, seconds_elapsed,lnd2atm_inst%t_rad_grc, info)          ! "CLM_INFRA"
-!    aux_buffer = lnd2atm_inst%albd_grc(:,1)+lnd2atm_inst%albd_grc(:,2)
-!    call oasis_put(2, seconds_elapsed,aux_buffer, info)     ! "CLM_ALBED"
-!    aux_buffer = 
-!    call oasis_put(3, seconds_elapsed,aux_buffer, info)     ! "CLM_ALBEI"
-     call oasis_put(2, seconds_elapsed,lnd2atm_inst%albd_grc(:,1), info)     ! "CLM_ALBED"
-     call oasis_put(3, seconds_elapsed,lnd2atm_inst%albi_grc(:,1), info)     ! "CLM_ALBEI"
-     call oasis_put(4, seconds_elapsed,lnd2atm_inst%taux_grc, info)          ! "CLM_TAUX"
-     call oasis_put(5, seconds_elapsed,lnd2atm_inst%tauy_grc, info)          ! "CLM_TAUY"
-     call oasis_put(6, seconds_elapsed,lnd2atm_inst%eflx_sh_tot_grc, info)   ! "CLM_SHFLX"
-     call oasis_put(7, seconds_elapsed,lnd2atm_inst%eflx_lh_tot_grc, info)   ! "CLM_LHFLX"
-     call oasis_put(8, seconds_elapsed,lnd2atm_inst%t_rad_grc, info)         ! "CLM_TGRND"
+     call oasis_put(12, seconds_elapsed,lnd2atm_inst%t_rad_grc, info)         ! "CLM_INFRA"
+     call oasis_put(13, seconds_elapsed,lnd2atm_inst%albd_grc(:,1), info)     ! "CLM_ALBED"
+     call oasis_put(14, seconds_elapsed,lnd2atm_inst%albi_grc(:,1), info)     ! "CLM_ALBEI"
+     call oasis_put(15, seconds_elapsed,lnd2atm_inst%taux_grc, info)          ! "CLM_TAUX"
+     call oasis_put(16, seconds_elapsed,lnd2atm_inst%tauy_grc, info)          ! "CLM_TAUY"
+     call oasis_put(17, seconds_elapsed,lnd2atm_inst%eflx_sh_tot_grc, info)   ! "CLM_SHFLX"
+     call oasis_put(18, seconds_elapsed,lnd2atm_inst%eflx_lh_tot_grc, info)   ! "CLM_LHFLX"
+     call oasis_put(19, seconds_elapsed,lnd2atm_inst%t_sf_grc, info)          ! "CLM_TGRND"
 
   end subroutine oas_send_icon
 
