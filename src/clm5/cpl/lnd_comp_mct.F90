b4fd2ad0ad29ac87c86d5f480e26c956af2e40f8
Johannes Keller <jo.keller@fz-juelich.de>
2024-08-01 16:03:14 +0200

    debug output in `lnd_resume` issue

--- src/clm5/cpl/lnd_comp_mct.F90
+++ src/clm5/cpl/lnd_comp_mct.F90
@@ -710,6 +710,7 @@ contains
     use seq_comm_mct     , only : num_inst_lnd
     use clm_varctl       , only : iulog
     use clm_varctl       , only : inst_index
+    use spmdMod     , only: iam
     implicit none
     ! !ARGUMENTS:
     type(seq_infodata_type), intent(IN) :: infodata     ! CESM driver level info data
@@ -720,6 +721,23 @@ contains
 
     call seq_infodata_GetData(infodata, lnd_resume=lnd_resume )
     ! If lnd_resume is blank, restart file wasn't modified
+
+    write(iulog,"(a,i10,i10,i10)") 'eCLM(lnd_comp_mct): iam, inst_index, num_inst_lnd ', iam, inst_index, num_inst_lnd
+    write(iulog,"(a,i10,i10)") 'eCLM(lnd_comp_mct): iam, min(inst_index, num_inst_lnd) ', iam, min(num_inst_lnd,inst_index)
+    write(iulog,"(a,i10,i10)") 'eCLM(lnd_comp_mct): iam, SHR_KIND_CL ', iam, SHR_KIND_CL
+    write(iulog,*) 'eCLM(lnd_comp_mct): iam, lnd_resume(1) ', iam, lnd_resume(1)
+    write(iulog,"(a,i10,i10)") 'eCLM(lnd_comp_mct): iam, len(lnd_resume(1)) ', iam, len(lnd_resume(1))
+    write(iulog,"(a,i10,i10)") 'eCLM(lnd_comp_mct): iam, len_trim(lnd_resume(1)) ', iam, len_trim(lnd_resume(1))
+    write(iulog,*) 'eCLM(lnd_comp_mct): iam, lnd_resume(num_inst_lnd) ', iam, lnd_resume(num_inst_lnd)
+    write(iulog,"(a,i10,i10)") 'eCLM(lnd_comp_mct): iam, len(lnd_resume(num_inst_lnd)) ', iam, len(lnd_resume(num_inst_lnd))
+    write(iulog,"(a,i10,i10)") 'eCLM(lnd_comp_mct): iam, len_trim(lnd_resume(num_inst_lnd)) ', iam, len_trim(lnd_resume(num_inst_lnd))
+    write(iulog,*) 'eCLM(lnd_comp_mct): iam, lnd_resume(min(num_inst_lnd,inst_index)) ', iam, lnd_resume(min(num_inst_lnd,inst_index))
+    write(iulog,"(a,i10,i10)") 'eCLM(lnd_comp_mct): iam, len(lnd_resume(min(num_inst_lnd,inst_index))) ', iam, len(lnd_resume(min(num_inst_lnd,inst_index)))
+    write(iulog,"(a,i10,i10)") 'eCLM(lnd_comp_mct): iam, len_trim(lnd_resume(min(num_inst_lnd,inst_index))) ', iam, len_trim(lnd_resume(min(num_inst_lnd,inst_index)))
+    ! write(iulog,*) 'eCLM(lnd_comp_mct): iam, lnd_resume(0) ', iam, lnd_resume(0)
+    ! write(iulog,"(a,i10,i10)") 'eCLM(lnd_comp_mct): iam, len(lnd_resume(0)) ', iam, len(lnd_resume(0))
+    ! write(iulog,"(a,i10,i10)") 'eCLM(lnd_comp_mct): iam, len_trim(lnd_resume(0)) ', iam, len_trim(lnd_resume(0))
+
     if ( len_trim(lnd_resume(min(num_inst_lnd,inst_index))) == 0 )then
        resume_from_data_assim = .false.
     ! Otherwise restart was modified and we are resuming from data assimulation
